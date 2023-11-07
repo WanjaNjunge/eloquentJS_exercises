c957a77f7bd9249970eacb79237945f9e3c5c0d3
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-11-07 16:59:57 +0300

    Done with ChessBoard+Chapter2

---
+++ chessBoard.js
@@ -0,0 +1,35 @@
+/*
+Chessboard 
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+Passing this string to console.log should show something like this:
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for anysize, outputting a grid of the given width and height.
+*/
+
+function createCheeseBoard(size) {
+    let board = '';
+
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            if ((i + j) % 2 === 0) {
+                board += ' ';
+            } else {
+                board += '#';
+            }
+        }
+        board += '\n';
+    }
+    return board;
+}
+
+let size = 8;
+console.log(createCheeseBoard(size));
+
